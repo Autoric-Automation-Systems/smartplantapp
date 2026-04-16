f1f9e0a5534a06f596f4fc5b9caa915eef6e5a33
Ruy Junior <ruyjunior21@gmail.com>
2026-04-15 22:27:34 -0300

    New creat configs function and edit forms - v5

--- src/app/(private)/dashboard/components/cardEvents.tsx
+++ src/app/(private)/dashboard/components/cardEvents.tsx
@@ -58,7 +58,7 @@ export default function CardEvents({
       return (
         <>
           <div onClick={() => setOpenHumidity(true)} className="cursor-pointer">
-            <HumidityCard value={value} />
+            <HumidityCard value={value} min={min} max={max} />
           </div>
 
           {/* MODAL */}
