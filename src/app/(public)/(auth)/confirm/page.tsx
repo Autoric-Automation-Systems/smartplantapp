04be256ffcc6ffa2bd19168fdac9c0d4e912d38a
Ruy Junior <ruyjunior21@hpprobook.autoric>
2026-08-28 19:16:47 -0300

    Update Reset Pass Page 3

--- src/app/(public)/(auth)/confirm/page.tsx
+++ src/app/(public)/(auth)/confirm/page.tsx
@@ -1,7 +1,10 @@
 import ConfirmPasswordForm from "@/components/auth/ConfirmPasswordForm";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
+    <Suspense fallback={<div className="p-4 text-sm text-gray-500">Loading...</div>}>
       <ConfirmPasswordForm />
+    </Suspense>
   );
 }
